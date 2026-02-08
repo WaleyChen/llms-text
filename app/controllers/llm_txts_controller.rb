a814f69ac3f1190520b29b4c60af6b57e219b103
Waley Chen <waleycz@gmail.com>
2026-02-08 14:17:48 -0500

    misc changes

--- app/controllers/llm_txts_controller.rb
+++
@@ -1,8 +0,0 @@
-# frozen_string_literal: true
-
-class LlmTxtsController < ApplicationController
-  def create
-    # No persistence for now — just receive the POST
-    redirect_to root_path
-  end
-end

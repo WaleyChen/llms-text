e4fffaf8be3e4ab1d9c9dc7b7e0f35164ecfadea
Waley Chen <waleycz@gmail.com>
2026-02-08 13:34:08 -0500

    front-end/generate-btn hits backend

--- app/controllers/llm_txts_controller.rb
+++ app/controllers/llm_txts_controller.rb
@@ -1,2 +1,8 @@
+# frozen_string_literal: true
+
 class LlmTxtsController < ApplicationController
+  def create
+    # No persistence for now — just receive the POST
+    redirect_to root_path
+  end
 end

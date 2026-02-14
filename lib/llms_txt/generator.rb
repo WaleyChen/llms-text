295ae8189bc5c1d9cd82f30f0d80f96da129eef1
Waley Chen <waleycz@gmail.com>
2026-02-14 18:51:25 -0500

    remove summary

--- lib/llms_txt/generator.rb
+++ lib/llms_txt/generator.rb
@@ -14,13 +14,6 @@ module LlmsTxt
       lines << "> #{@homepage[:description] || 'No description available.'}"
       lines << ""
 
-      # Add any additional context from homepage content
-      if @homepage[:content].present?
-        summary = summarize(@homepage[:content], max_length: 200)
-        lines << summary if summary.present?
-        lines << ""
-      end
-
       # Group pages by section
       docs = @pages.select { |p| p[:url].match?(%r{/(docs?|documentation|api|guide|tutorial)}i) }
       other = @pages.reject { |p| docs.include?(p) || p == @homepage }
@@ -48,19 +41,6 @@ module LlmsTxt
     end
 
     private
-
-    def summarize(text, max_length:)
-      return nil if text.blank?
-
-      text = text.strip
-      return text if text.length <= max_length
-
-      # Try to cut at sentence boundary
-      truncated = text[0, max_length]
-      last_period = truncated.rindex(/[.!?]\s/)
-      truncated = truncated[0, last_period + 1] if last_period
-      truncated + "..."
-    end
 
     def extract_title_from_url(url)
       uri = URI.parse(url)

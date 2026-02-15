31b87aa29de1756633f3c4aefffd83c592d8cd1c
Waley Chen <waleycz@gmail.com>
2026-02-14 20:26:11 -0500

    update generate logic for careers and blog

--- lib/llms_txt/generator.rb
+++ lib/llms_txt/generator.rb
@@ -15,8 +15,10 @@ module LlmsTxt
       lines << ""
 
       # Group pages by section
+      careers = @pages.select { |p| p[:url].match?(%r{/(career|careers)}i) }
       docs = @pages.select { |p| p[:url].match?(%r{/(docs?|documentation|api|guide|tutorial)}i) }
-      other = @pages.reject { |p| docs.include?(p) || p == @homepage }
+      blog = @pages.select { |p| p[:url].match?(%r{/(blog)}i) }
+      other = @pages.reject { |p| docs.include?(p) || careers.include?(p) || blog.include?(p) || p == @homepage }
 
       if docs.any?
         lines << "## Documentation"
@@ -28,10 +30,30 @@ module LlmsTxt
         lines << ""
       end
 
+      if careers.any?
+        lines << "## Careers"
+        lines << ""
+        careers.each do |page|
+          title = page[:title] || extract_title_from_url(page[:url])
+          lines << "- [#{title}](#{page[:url]})"
+        end
+        lines << ""
+      end
+
+      if blog.any?
+        lines << "## Blog"
+        lines << ""
+        blog.each do |page|
+          title = page[:title] || extract_title_from_url(page[:url])
+          lines << "- [#{title}](#{page[:url]})"
+        end
+        lines << ""
+      end
+
       if other.any?
         lines << "## Additional Resources"
         lines << ""
-        other.first(10).each do |page|
+        other.each do |page|
           title = page[:title] || extract_title_from_url(page[:url])
           lines << "- [#{title}](#{page[:url]})"
         end

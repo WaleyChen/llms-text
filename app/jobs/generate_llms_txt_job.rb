6770866da2c0bc319e6d6e614f8ed32b00692116
Waley Chen <waleycz@gmail.com>
2026-02-19 22:12:34 -0500

    add db txs for run & runconfig

--- app/jobs/generate_llms_txt_job.rb
+++ app/jobs/generate_llms_txt_job.rb
@@ -3,24 +3,32 @@ class GenerateLlmsTxtJob < ApplicationJob
 
   def perform(run_id)
     run = Run.find(run_id)
-    run.run_config.start_generating
-    run.start
+    ActiveRecord::Base.transaction do
+      run.run_config.start_generating
+      run.start
+    end
 
     # Get the crawl result from the cache
     crawl = Rails.cache.read(LlmsTxt::Crawler.run_crawl_cache_key(run))
     unless crawl
-      run.run_config.fail
-      run.fail
+      ActiveRecord::Base.transaction do
+        run.run_config.fail
+        run.fail
+      end
       raise "Crawl result expired or not found for run_config #{run.run_config_id}"
     end
 
     LlmsTxt::Generator.new(run, crawl[:pages], crawl[:failed_pages]).generate
-    run.complete
-    run.run_config.complete
+    ActiveRecord::Base.transaction do
+      run.complete
+      run.run_config.complete
+    end
   rescue StandardError => e
-    run.append_debug_logs("GenerateLlmsTxtJob Error: #{e.message}")
-    run&.fail # Fail the run
-    run.run_config.fail
+    ActiveRecord::Base.transaction do
+      run.append_debug_logs("GenerateLlmsTxtJob Error: #{e.message}") if run
+      run&.fail
+      run.run_config.fail if run
+    end
     raise e
   end
 end
